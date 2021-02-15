583d82f76ca0b573ea6861dd92fa163c629b639f
Daniel Miller <danielemiller@live.com>
2021-02-14 23:13:08 -0500

    Create and style top sale section

--- index.js
+++ index.js
@@ -5,5 +5,23 @@ $(document).ready(function(){
       dots: true,
       items: 1
   })
-    
-})
+  
+  //top sale owl carousel
+  $("#top-sale .owl-carousel").owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      }, 
+      1000: {
+        items: 5
+      }
+    }
+  })
+
+});
