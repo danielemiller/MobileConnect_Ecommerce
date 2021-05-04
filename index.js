843897d93b0cb7f27e9562f21359634da4c320d2
Daniel Miller <danielemiller@live.com>
2021-05-04 15:23:50 -0400

    Create and style special price section

--- index.js
+++ index.js
@@ -22,6 +22,21 @@ $(document).ready(function(){
         items: 5
       }
     }
+  });
+
+  //isotope filter
+  var $grid = $(".grid").isotope({
+    itemSelector:'.grid-item',
+    layoutMode:'fitRows'
+  });
+
+  //filter items on button click
+  $(".button-group").on("click", "button", function(){
+    var filterValue = $(this).attr('data-filter')
+    $grid.isotope({ filter: filterValue});
   })
+
+
+
 
 });
